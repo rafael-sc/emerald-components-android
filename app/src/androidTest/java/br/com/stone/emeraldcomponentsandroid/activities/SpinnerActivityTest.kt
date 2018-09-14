62b7bce28fcd7215e7b7c52c566a9063932b5724
diegolucasb <diegolucasb@gmail.com>
2018-09-14 18:40:42 -0300

    :white_check_mark: Add SpinnerActivity UI tests

---
+++ app/src/androidTest/java/br/com/stone/emeraldcomponentsandroid/activities/SpinnerActivityTest.kt
@@ -0,0 +1,102 @@
+package br.com.stone.emeraldcomponentsandroid.activities
+
+import android.support.test.espresso.Espresso.onData
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.closeSoftKeyboard
+import android.support.test.espresso.action.ViewActions.replaceText
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withClassName
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.view.View
+import android.view.ViewGroup
+import br.com.stone.emeraldcomponentsandroid.R
+import org.hamcrest.CoreMatchers.anything
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.AllOf.allOf
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+/**
+ * Created by diegolucasb on 14/09/18.
+ * Copyright (c) Stone Co. All rights reserved.
+ * lucas.amaral@stone.com.br
+ */
+@RunWith(AndroidJUnit4::class)
+class SpinnerActivityTest {
+    @get:Rule
+    val activityRule = ActivityTestRule(SpinnerActivity::class.java)
+
+    @Test
+    fun shouldAutoCompleteValidateTypedValue() {
+        val autoCompleteView = onView(allOf(
+                withId(R.id.emeraldAutoCompleteView),
+                childAtPosition(childAtPosition(withId(R.id.autoCompleteView),
+                        0),
+                        0),
+                isDisplayed()))
+
+        autoCompleteView.perform(click())
+
+        val chosenValue = "100 - Aaaaaaaaa"
+        autoCompleteView.perform(replaceText(chosenValue), closeSoftKeyboard())
+                .check(matches(withText(chosenValue)))
+    }
+
+    @Test
+    fun shouldSpinnerMatchTestWhenUserClickInOneOption() {
+        onView(allOf<View>(
+                withId(R.id.emeraldSpinner),
+                childAtPosition(
+                        childAtPosition(
+                                withId(android.R.id.content),
+                                0),
+                                0),
+                isDisplayed()))
+                .perform(click())
+
+        val appCompatTextView = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(`is`<String>("android.widget.PopupWindow\$PopupBackgroundView")),
+                        0))
+                .atPosition(1)
+        appCompatTextView.perform(click())
+
+        val expectedOption = "Option 2"
+        onView(allOf<View>(
+                withId(android.R.id.text1), withText(expectedOption),
+                childAtPosition(allOf<View>(
+                        withId(R.id.emeraldSpinner),
+                        childAtPosition(
+                                IsInstanceOf.instanceOf(android.view.ViewGroup::class.java),
+                                0)),
+                                0),
+                        isDisplayed()))
+        .check(matches(withText(expectedOption)))
+    }
+
+    private fun childAtPosition(
+            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {}
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return (parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position))
+            }
+        }
+    }
+
+}
--- app/src/main/java/br/com/stone/emeraldcomponentsandroid/activities/SpinnerActivity.kt
+++ app/src/main/java/br/com/stone/emeraldcomponentsandroid/activities/SpinnerActivity.kt
@@ -2,6 +2,7 @@ package br.com.stone.emeraldcomponentsandroid.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import br.com.stone.emeraldcomponentsandroid.R
 import kotlinx.android.synthetic.main.activity_spinner.*
 
@@ -10,6 +11,27 @@ class SpinnerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spinner)
-        emeraldSpinner.setItems(listOf("Visa","Master"))
+        emeraldSpinner.setItems(listOf("Option 1","Option 2"))
+
+        autoCompleteView.setItems(setOf(
+                Value("100", "Aaaaaaaaa"),
+                Value("200", "Bbbbbbbbb"),
+                Value("300", "Ccccccccc"),
+                Value("400", "Ddddddddd"),
+                Value("500", "Eeeeeeeee"),
+                Value("600", "Fffffffff"),
+                Value("700", "Ggggggggg"),
+                Value("800", "Hhhhhhhhh"),
+                Value("900", "Ijklmnopq")),
+                callback = {
+                    Log.i("AUTOCOMPLETE", it.desc)
+                })
+    }
+
+
+    data class Value(val id: String, val desc: String) {
+        override fun toString(): String {
+            return "$id - $desc"
+        }
     }
 }
