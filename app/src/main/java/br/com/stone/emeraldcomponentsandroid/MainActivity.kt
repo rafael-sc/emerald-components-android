4bcfec7bd56ede198351701869d3d08232c6f733
diegolucasb <diegolucasb@gmail.com>
2018-09-14 17:33:24 -0300

    :art: Changes activities that will be called by MainActivity

--- app/src/main/java/br/com/stone/emeraldcomponentsandroid/MainActivity.kt
+++ app/src/main/java/br/com/stone/emeraldcomponentsandroid/MainActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import br.com.stone.emeraldcomponents.extension.setUp
+import br.com.stone.emeraldcomponentsandroid.activities.ButtonActivity
+import br.com.stone.emeraldcomponentsandroid.activities.CalendarActivity
+import br.com.stone.emeraldcomponentsandroid.activities.DateActivity
 import br.com.stone.emeraldcomponentsandroid.activities.InputActivity
+import br.com.stone.emeraldcomponentsandroid.activities.SpinnerActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_activity_list.view.*
 
@@ -15,8 +19,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val activities = listOf(
-                InputActivity::class.java to "Inputs"
-        )
+                ButtonActivity::class.java to "Button",
+                CalendarActivity::class.java to "Calendar + Date Event List",
+                InputActivity::class.java to "Inputs",
+                SpinnerActivity::class.java to "Spinner",
+                DateActivity::class.java to "Date Selector and Filter")
 
         recyclerMain.setUp(activities,
                 { R.layout.item_activity_list },
