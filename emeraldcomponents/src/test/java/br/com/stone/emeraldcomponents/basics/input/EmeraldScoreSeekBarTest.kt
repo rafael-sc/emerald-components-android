1ff664d20c317df580249b4dd2cd91fda5eab56f
Daniele Freitas <danifreitas183@gmail.com>
2020-04-29 15:22:21 -0300

    :white_check_mark: Adds EmeraldScoreSeekBarTest

---
+++ emeraldcomponents/src/test/java/br/com/stone/emeraldcomponents/basics/input/EmeraldScoreSeekBarTest.kt
@@ -0,0 +1,47 @@
+package br.com.stone.emeraldcomponents.basics.input
+
+import androidx.test.core.app.ApplicationProvider
+import br.com.stone.emeraldcomponents.basic.seekBar.EmeraldScoreSeekBar
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+
+/**
+ * Created by daniele.freitas on 29/04/20.
+ * Copyright (c) Stone Co. All rights reserved.
+ * daniele.freitas@stone.com.br
+ */
+
+@RunWith(RobolectricTestRunner::class)
+class EmeraldScoreSeekBarTest {
+
+    lateinit var scoreSeekBar: EmeraldScoreSeekBar
+
+    @Before
+    fun setup() {
+        scoreSeekBar = EmeraldScoreSeekBar(ApplicationProvider.getApplicationContext())
+    }
+
+    @Test
+    fun testInstanceWithContext() {
+        val view = EmeraldScoreSeekBar(ApplicationProvider.getApplicationContext())
+        Assert.assertNotNull(view)
+    }
+
+    @Test
+    fun testInstanceWithAttributeSet() {
+        val attrs = Robolectric.buildAttributeSet()
+                .build()
+        val view = EmeraldScoreSeekBar(ApplicationProvider.getApplicationContext(), attrs)
+        Assert.assertNotNull(view)
+    }
+
+    @Test
+    fun testProgress() {
+        scoreSeekBar.progress = 5
+        Assert.assertEquals(scoreSeekBar.progress, 5)
+    }
+}
